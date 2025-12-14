d9c5d71b1e470a679affab9970b57c87121573d0
Santiago F <forerovar.santiago07@gmail.com>
2025-12-14 15:02:44 -0500

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -10,11 +10,11 @@ function love.conf(t)
     t.window.title = "Morel" -- nombre del juego en la ventana
     --t.window.icon = "Icon.jpg" -- le pone icono a la aplicación
     -- NO establecemos width/height fijos, dejamos que fullscreen use la resolución nativa
-    --t.window.resizable = true -- permite cambiar el tamaño de ventana
+    t.window.resizable = true -- permite cambiar el tamaño de ventana
     --t.window.minwidth = 200 -- pone limite
     --t.window.minheight = 200 -- pone limite
-    t.window.borderless = true -- le quita el borde de arriba
-    t.window.fullscreen = true -- pues fullscreen
+    t.window.borderless = false -- le quita el borde de arriba
+    t.window.fullscreen = false -- pues fullscreen
     --t.window.fullscreentype = "desktop" -- usa la resolución nativa del monitor
     t.window.display = 1
     --t.window.vsync = 1 -- establece vysinc
